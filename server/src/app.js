c4138038dc64ca4b7c28260b590d8994fa6cf010
Daniel Ryu <rujjiryu99@gmail.com>
2020-09-19 14:00:49 -0700

    implement catch all route in server to enable client side routing

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+// middleware
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// catch all route for serving index.html despite the requested route. Ensures react routing still works on client side.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 module.exports = app;
