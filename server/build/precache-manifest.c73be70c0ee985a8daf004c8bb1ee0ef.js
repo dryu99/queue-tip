c1b0686393af1e39c4c4bca444d1033fd5195a36
Daniel Ryu <rujjiryu99@gmail.com>
2020-09-20 22:23:45 -0700

    uibuild

---
+++ server/build/precache-manifest.c73be70c0ee985a8daf004c8bb1ee0ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8e6659f456e3928530fd1678985a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fcb9653bb412559f2f3",
+    "url": "/static/js/2.f9f9331c.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.f9f9331c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d80d158f35584ebf6e",
+    "url": "/static/js/main.aaf34ecd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
