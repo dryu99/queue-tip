bbcd91f3e492fa847413d6c1eef2a0aa3aac9768
Daniel Ryu <rujjiryu99@gmail.com>
2020-09-26 04:34:15 -0700

    update footer url

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <Navbar sticky="bottom">
       <Container className="mt-5 mb-2 justify-content-center">
         <small className="text-muted">
-          <i><a href={'https://github.com/dryu99'}>Daniel Ryu</a> 2020</i>
+          <i><a href={'https://en.wikipedia.org/wiki/Daniel_(Elton_John_song)'}>Daniel Ryu</a> 2020</i>
         </small>
       </Container>
     </Navbar>
