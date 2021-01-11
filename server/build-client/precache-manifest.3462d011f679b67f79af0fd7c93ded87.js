790ca83ca1e33ab267c41c8c72b04d238da7216c
Daniel Ryu <rujjiryu99@gmail.com>
2021-01-11 00:33:16 -0800

    build

--- server/build-client/precache-manifest.3462d011f679b67f79af0fd7c93ded87.js
+++ server/build-client/precache-manifest.3462d011f679b67f79af0fd7c93ded87.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e09aafe4a4aceb501197770b46333b1",
+    "revision": "bb2b23cd625cb3adf1ba1c737764324e",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.db856f8d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "148702c44f3df45db10c",
-    "url": "/static/js/main.73452625.chunk.js"
+    "revision": "9b3bc4be126029412e70",
+    "url": "/static/js/main.fa27b968.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- server/build-client/service-worker.js
+++ server/build-client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31f69b7b7ae04b32acee863f7282bd0c.js"
+  "/precache-manifest.3462d011f679b67f79af0fd7c93ded87.js"
 );
 
 self.addEventListener('message', (event) => {
